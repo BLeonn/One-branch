393efa91b107d961ff8d64c50817e55a95524c5f
BLeonn <87141077+BLeonn@users.noreply.github.com>
2024-01-14 12:37:50 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './Navbar.jsx';
+
+
+function App() {
+  return (
+    
+    <div className="App">
+     <h1>hellow world</h1>
+    </div>
+  );
+}
+
+export default App;
